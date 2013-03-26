cd687babbe6b27db00adcd1bef9cb01345e95a5f
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-03-26 23:53:32 +0900

    Add googletest sources to project.

---
+++ tests/testHMDatabase.cpp
@@ -0,0 +1,9 @@
+//
+//  testHMDatabase.cpp
+//  hmdb
+//
+//  Created by matsuki hidenori on 3/26/13.
+//
+//
+
+#include "testHMDatabase.h"
---
+++ tests/testHMDatabase.h
@@ -0,0 +1,14 @@
+//
+//  testHMDatabase.h
+//  hmdb
+//
+//  Created by matsuki hidenori on 3/26/13.
+//
+//
+
+#ifndef __hmdb__testHMDatabase__
+#define __hmdb__testHMDatabase__
+
+#include <iostream>
+
+#endif /* defined(__hmdb__testHMDatabase__) */
